ce8f166a4a9a780874d7e2db8b7f65008a61e79d
qor0923 <qorehdfkr@naver.com>
2023-12-18 10:29:27 +0900

    Initial commit

---
+++ src/main/java/com/example/redistest/controller/RedisController.java
@@ -0,0 +1,8 @@
+package com.example.redistest.controller;/**
+ * Please explain the class!!
+ *
+ * @fileName      : RedisController
+ * @author        : kma04
+ * @since         : 2023-12-15
+ */public class RedisController {
+}
---
+++ src/main/java/com/example/redistest/dto/RedisReqDto.java
@@ -0,0 +1,8 @@
+package com.example.redistest.dto;/**
+ * Please explain the class!!
+ *
+ * @fileName      : RedisReqDto
+ * @author        : kma04
+ * @since         : 2023-12-15
+ */public class RedisReqDto {
+}
